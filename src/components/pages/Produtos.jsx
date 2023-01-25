47617705ee40dcb7bbe4b0578d4e7cf22e63f41d
pevehdev <pauloht2020@gmail.com>
2023-01-25 08:56:07 -0300

    Colocando CSS Grid nos itens para que sejam exibidos em 4 colunas

--- src/components/pages/Produtos.jsx
+++ src/components/pages/Produtos.jsx
@@ -13,24 +13,45 @@ export default function Produtos() {
       .catch(error => console.error(error))
   }, [])
 
+  const [bebidas, setBebidas] = useState(null);
+  useEffect(() => {
+    axios.get('https://produtos.onrender.com/Bebidas')
+      .then(response => setBebidas(response.data))
+      .catch(error => console.error(error))
+  }, [])
+
+  const [acompanhamentos, setAcompanhamentos] = useState(null);
+  useEffect(() => {
+    axios.get('https://produtos.onrender.com/Acompanhamentos')
+      .then(response => setAcompanhamentos(response.data))
+      .catch(error => console.error(error))
+  }, [])
+
+  const [combos, setCombos] = useState(null);
+  useEffect(() => {
+    axios.get('https://produtos.onrender.com/Combos')
+      .then(response => setCombos(response.data))
+      .catch(error => console.error(error))
+  }, [])
+
   return (
-    <>
-    <h2>Hamburguers</h2>
+    <div>
+      <h2 className="tituloProduto">Hamburguers</h2>
       <div >
         {hamburguers ? (
-          
-          <div className='content'>
-            
+
+          <div className='content product-grid'>
+
             {hamburguers.map(hamburguer => (
               <div className="product-card" key={hamburguer.id}>
                 <div className="card-img">
                   <img src={Imagem} alt="My Image" />
                 </div>
                 <div>
-                <h3>{hamburguer.nome}</h3>
-                <p>{hamburguer.descricao}</p>
-                <p>R${hamburguer.preco}</p>
-              </div>
+                  <h3>{hamburguer.nome}</h3>
+                  <p>{hamburguer.descricao}</p>
+                  <p>R${hamburguer.preco}</p>
+                </div>
               </div>
             ))}
           </div>
@@ -38,6 +59,96 @@ export default function Produtos() {
           <div>Loading...</div>
         )}
       </div>
-    </>
+
+      <br />
+      <br />
+      <hr />
+      <br />
+      <br />
+
+      <h2 className="tituloProduto">Bebidas</h2>
+      <div >
+        {bebidas ? (
+
+          <div className='content product-grid'>
+
+            {bebidas.map(bebida => (
+              <div className="product-card" key={bebida.id}>
+                <div className="card-img">
+                  <img src={Imagem} alt="My Image" />
+                </div>
+                <div>
+                  <h3>{bebida.nome}</h3>
+                  <p>{bebida.volume}</p>
+                  <p>R${bebida.preco}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+
+      <br />
+      <hr />
+      <br />
+      <br />
+
+
+      <h2 className="tituloProduto ">Acompanhamentos</h2>
+      <div >
+        {acompanhamentos ? (
+
+          <div className='content product-grid'>
+
+            {acompanhamentos.map(acompanhamento => (
+              <div className="product-card" key={acompanhamento.id}>
+                <div className="card-img">
+                  <img src={Imagem} alt="My Image" />
+                </div>
+                <div>
+                  <h3>{acompanhamento.nome}</h3>
+                  <p>{acompanhamento.volume}</p>
+                  <p>R${acompanhamento.preco}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+
+      <br />
+      <hr />
+      <br />
+      <br />
+
+      <h2 className="tituloProduto">Combos</h2>
+      <div >
+        {combos ? (
+
+          <div className='content product-grid'>
+
+            {combos.map(combo => (
+              <div className="product-card" key={combo.id}>
+                <div className="card-img">
+                  <img src={Imagem} alt="My Image" />
+                </div>
+                <div>
+                  <h3>{combo.nome}</h3>
+                  <p>{combo.volume}</p>
+                  <p>R${combo.preco}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>Loading...</div>
+        )}
+      </div>
+
+    </div>
   )
 }
