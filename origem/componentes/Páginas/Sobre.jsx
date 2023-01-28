c88e7c735bf7168321aeb97d104f607ddb47e473
Victória Carvalho <victoria.carvalho868@outlook.com>
2023-01-27 21:01:29 -0300

    Atualizando a página sobre nós

---
+++ origem/componentes/Páginas/Sobre.jsx
@@ -0,0 +1,17 @@
+import Login from "../styles/Login.css";
+export default function Sobre(){
+  return(
+    <div> 
+     
+     <div className="bloco1"> <p className="P1"> A one Burguer foi inaugurada no ano de 2020, na cidade do Rio de Janeiro afim de inovar e oferecer o melhor produto para os nosos clientes. </p> 
+      <div className="imagem1"> 
+<img src="../imagens\HAMBURGUER.jpg" className="imagem1" ></img>
+    </div>
+</div>
+      
+       <div> <p className="P2"> Nossos hambúrgueres além de suculentos são feitos com ingredientes selecionados feitos diretamente na chapa fora variedade combos, porções e bebidas.</p></div>
+       </div>
+
+       
+  )
+}
