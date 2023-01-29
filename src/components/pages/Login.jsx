6592c8869fd8c4c31987da841a104551f14295d7
SamaraCSSLima <114176563+SamaraCSSLima@users.noreply.github.com>
2023-01-29 17:51:37 -0300

    Pagina de Login atualizada

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,6 +1,52 @@
-export default function Login(){
-  return(
-    <>
-    </>
-  )
+import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import "../styles/Login.css"; 
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // Verifique se o email e senha correspondem aos valores esperados
+    if (email === 'admin@exemplo.com' && password === 'admin') {
+      // Autenticação bem-sucedida, redirecionar o usuário para a página protegida
+      window.location.href = '/Cadastro';
+    } else {
+      // Exibir mensagem de erro
+      alert('Email ou senha inválido');
+    }
+  }
+
+  return (
+    <form className="formulario" onSubmit={handleSubmit}>
+      <h1> Login</h1>
+      <label>
+        Email:
+        <br /> <input className='camposLogin'
+          type="email"
+          value={email}
+          onChange={event => setEmail(event.target.value)}
+        /> 
+      </label>
+      <br />
+      <label>
+        Senha:
+        <br />
+        <input className='camposLogin'
+          type="password"
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+      </label>
+      <br />
+      <Link to="/cadastroprodutos">
+      <button className="botaoLogin" type="submit">Entrar</button>
+      </Link>
+      
+    </form>
+  );
 }
+
+export default LoginForm;
