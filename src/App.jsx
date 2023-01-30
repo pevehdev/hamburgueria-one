dbb31422ff728cb42ac004ca9ee8a9a31743891a
pevehdev <pauloht2020@gmail.com>
2023-01-30 03:40:50 -0300

    Ajustando margin e altura do body

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route, Outlet} from 'react-router-dom'
 import Footer from './components/footer.jsx/Footer.jsx';
 import Navbar from './components/navbar/Navbar.jsx';
+import './App.css';
 
 function App() {
   
